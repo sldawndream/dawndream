d31fb1efc1d4a7da5f036c7b7c253734176b4c8e
sldawndream <sldawndream@gmail.com>
2026-03-28 22:50:06 -0300

    Add files via upload

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,21 @@
+import Link from 'next/link';
 import styles from './Navbar.module.css';
 
-export default function Navbar() {
+export default function Navbar({ activePage }) {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <span className={styles.drop}></span>
         DAWNDREAM
       </div>
+      <nav className={styles.nav}>
+        <Link href="/" className={`${styles.navLink} ${!activePage || activePage === 'lore' ? styles.active : ''}`}>
+          Lore & History
+        </Link>
+        <Link href="/clans" className={`${styles.navLink} ${activePage === 'clans' ? styles.active : ''}`}>
+          Clans & Houses
+        </Link>
+      </nav>
       <div className={styles.right}>Second Life RPG System</div>
     </header>
   );

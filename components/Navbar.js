d35c3d80cd586a0eab50fe862b6b969198ba473a
sldawndream <sldawndream@gmail.com>
2026-04-02 03:39:15 +0100

    Add files via upload

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,21 +9,12 @@ export default function Navbar({ activePage }) {
         DAWNDREAM
       </div>
       <nav className={styles.nav}>
-        <Link href="/" className={`${styles.navLink} ${!activePage || activePage === 'lore' ? styles.active : ''}`}>
-          Lore & History
-        </Link>
-        <Link href="/clans" className={`${styles.navLink} ${activePage === 'clans' ? styles.active : ''}`}>
-          Clans & Houses
-        </Link>
-        <Link href="/hordes" className={`${styles.navLink} ${activePage === 'hordes' ? styles.active : ''}`}>
-          Hordes
-        </Link>
-        <Link href="/bloodlines" className={`${styles.navLink} ${activePage === 'bloodlines' ? styles.active : ''}`}>
-          Bloodlines
-        </Link>
-        <Link href="/gallery" className={`${styles.navLink} ${activePage === 'gallery' ? styles.active : ''}`}>
-          Gallery
-        </Link>
+        <Link href="/" className={`${styles.navLink} ${!activePage || activePage === 'lore' ? styles.active : ''}`}>Lore & History</Link>
+        <Link href="/clans" className={`${styles.navLink} ${activePage === 'clans' ? styles.active : ''}`}>Clans & Houses</Link>
+        <Link href="/hordes" className={`${styles.navLink} ${activePage === 'hordes' ? styles.active : ''}`}>Hordes</Link>
+        <Link href="/bloodlines" className={`${styles.navLink} ${activePage === 'bloodlines' ? styles.active : ''}`}>Bloodlines</Link>
+        <Link href="/chronicles" className={`${styles.navLink} ${activePage === 'chronicles' ? styles.active : ''}`}>Chronicles</Link>
+        <Link href="/gallery" className={`${styles.navLink} ${activePage === 'gallery' ? styles.active : ''}`}>Gallery</Link>
       </nav>
       <div className={styles.right}>Second Life RPG System</div>
     </header>
